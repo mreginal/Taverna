c82fe9ee83da2bcdace1e9f989678be1d10c8082
luannycholasdev <luan.nycholas@gmail.com>
2024-03-25 22:37:21 -0300

    Cadastro de usuário com senha encriptada

--- backend/src/controllers/user_controller.py
+++ backend/src/controllers/user_controller.py
@@ -1,22 +1,33 @@
 from flask import jsonify, request
 from models.user_model import User
 from app import client
+import bcrypt
 
 def cadastrar_usuario():
     data = request.json
     
-    if not all(key in data for key in ('name', 'birthdate', 'gender')):
+    if not all(key in data for key in ('name', 'birthdate', 'email', 'password', 'confirmpassword', 'gender')):
         return jsonify({'message': 'Campos obrigatórios ausentes'}), 400
+    
+    if data['confirmpassword'] != data['password']:
+        return jsonify({'message': 'A confirmação da senha está incorreta'}), 400
+    
+    password = data['password']
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     
     novo_usuario = User(
         name=data['name'],
         birthdate=data['birthdate'],
+        email=data['email'],
+        password=hashed,
         gender=data['gender']
     )
     
     client.taverna.usuarios.insert_one({
         'name': novo_usuario.name,
         'birthdate': novo_usuario.birthdate,
+        'email': novo_usuario.email,
+        'password': novo_usuario.password,
         'gender': novo_usuario.gender
     })
     
--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -1,5 +1,7 @@
 class User:
-    def __init__(self, name, birthdate, gender):
+    def __init__(self, name, birthdate, email, password, gender ):
         self.name = name
         self.birthdate = birthdate
+        self.email = email
+        self.password = password
         self.gender = gender
