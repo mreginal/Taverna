ba0d432a2c58e0577056c3688083820b7cfdb103
luannycholasdev <luan.nycholas@gmail.com>
2024-03-28 20:31:47 -0300

    Requirements e .env_examples

--- backend/src/models/user_model.py
+++ backend/src/models/user_model.py
@@ -3,12 +3,12 @@ import datetime
 
 class User:
     def __init__(
-            self, 
-            name: str, 
-            birthdate: Union[str, datetime.datetime], 
-            email:str, 
-            password, 
-            gender:Optional[str] 
+            self,
+            name: str,
+            birthdate: Union[str, datetime.datetime],
+            email:str,
+            password,
+            gender:Optional[str]
         ):
         self.name = name
         self.birthdate = birthdate
