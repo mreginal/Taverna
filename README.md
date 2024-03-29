3193ebf322e819533a3f60ed5e44ffeac950150e
Luan Nycholas <87026445+Nihkk@users.noreply.github.com>
2024-03-29 16:24:38 -0300

    Feature cadastro backend
    * Testando a branch de feature
    
    * O grande reset
    
    * Siu, receba, o melhor do mundo, obrigado meu deus
    
    * git ignore
    
    * Cadastro de usuário com senha encriptada
    
    * Pequenas alterações em user model e controller
    
    * Requirements e .env_examples
    
    * Flask env adicionado, importações corrigidas, flask run funcional
    
    * Deploy teste
    
    * Errei fui muleke

--- README.md
+++
@@ -1,2 +0,0 @@
-# BoaReceita
-Projeto de Gerenciamento de Projetos - Site (Rede Social) para cozinheiros chamado Boa Receita
---
+++ backend/src/controllers/user_controller.py
@@ -0,0 +1,31 @@
+from flask import jsonify, request
+from models.user_model import User
+from app import client
+import bcrypt
+
+def cadastrar_usuario():
+    data = request.json
+    
+    if not all(key in data for key in ('name', 'birthdate', 'email', 'password')):
+        return jsonify({'message': 'Campos obrigatórios ausentes'}), 400
+    
+    password = data['password']
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    novo_usuario = User(
+        name=data['name'],
+        birthdate=data['birthdate'],
+        email=data['email'],
+        password=hashed,
+        gender=data['gender']
+    )
+    
+    client.taverna.usuarios.insert_one({
+        'name': novo_usuario.name,
+        'birthdate': novo_usuario.birthdate,
+        'email': novo_usuario.email,
+        'password': novo_usuario.password,
+        'gender': novo_usuario.gender
+    })
+    
+    return jsonify({'message': 'Usuário cadastrado com sucesso!'}), 201
